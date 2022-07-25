af0dcda906ac5d4aea0fac68c0d9ce73ebabc380
Maxi <mmaximonchuk@gmail.com>
2022-07-25 11:26:38 +0300

    update: fix attemp

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { Filter, ContactList, Section, ContactForm } from './components';
 import { useEffect } from 'react';
 
 const INITIAL_CONTACTS_LIST = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
 const App = () => {
@@ -16,19 +16,44 @@ const App = () => {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    if (!!localStorage.getItem('contacts')) {
-      setContacts(JSON.parse(localStorage.getItem('contacts')));
-    }
+    if (!localStorage.getItem('contacts')) return;
+
+    setContacts(JSON.parse(localStorage.getItem('contacts')));
   }, []);
 
-  // useEffect(() => {
+  useEffect(() => {
     // TODO: Fix
-  //   localStorage.setItem(
-  //     'contacts',
-  //     JSON.stringify(contacts)
-  //   );
-  // }, [contacts]);
+    // localStorage.setItem(
+    //   'contacts',
+    //   JSON.stringify(contacts)
+    // );
+    // if (!!localStorage.getItem('contacts')) {
+    //   setContacts(JSON.parse(localStorage.getItem('contacts')));
+    // } else {
 
+    // }
+    // const isEqualContactsLength = contacts.length !== JSON.parse(localStorage.getItem('contacts'))?.length;
+
+    // if (isEqualContactsLength && contacts.length !== 0) {
+    //   localStorage.setItem('contacts', JSON.stringify(contacts));
+    //   console.log('contacts updated');
+    // }
+
+
+    /*
+    |
+    |
+    |
+    Это невозможно делать, я понимаю, что обращение к внешнему АПИ(в нашем случаек у ЛокалСторедж) 
+    это сайд эффект, но дело в том, что этот юзЕффект запускается при монитировании, когда 
+    он запускается не должен и я просто понятия не имею как можно прописать условие, чтобы оно
+    не выполнялось при первом рендере компонента, если подскажете как записать условие проверки
+    буду очень благодарен :)
+    */
+
+  }, [contacts]);
+
+ 
   const handleAddContact = newContactData => {
     const newContactEntity = {
       id: nanoid(),
@@ -47,13 +72,12 @@ const App = () => {
   };
 
   const handleDeleteContact = contactId => {
-    const newContacts = contacts.filter(contact => contact.id !== contactId);
-
-    setContacts(newContacts);
-    localStorage.setItem(
-      'contacts',
-      JSON.stringify(newContacts)
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
     );
+
+    const newContacs = contacts.filter(contact => contact.id !== contactId);
+    localStorage.setItem('contacts', JSON.stringify(newContacs));
   };
 
   const handleFilterContactsByName = ({ target: { value } }) => {
